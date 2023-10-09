8dfacafb1f70cc20c40fda2b3c66b616842501da
yugo-ibuki <y.ibuki91@gmail.com>
2023-10-10 01:10:48 +0900

    wip

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import * as ts from 'typescript'
+
+function inspectNode(node: ts.Node) {
+  console.log(
+    `Node kind: ${
+      ts.SyntaxKind[node.kind]
+    }, Start: ${node.getStart()}, End: ${node.getEnd()}`
+  )
+
+  ts.forEachChild(node, inspectNode)
+}
+
+function generateAST(sourceCode: string) {
+  const sourceFile = ts.createSourceFile(
+    'sample.ts',
+    sourceCode,
+    ts.ScriptTarget.Latest,
+    true
+  )
+  inspectNode(sourceFile)
+}
+
+const code = `
+function hello(name: string) {
+    console.log("Hello, " + name);
+}
+`
+
+generateAST(code)
